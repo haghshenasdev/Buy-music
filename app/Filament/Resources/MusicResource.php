cda8d9e17b187a9e00f48332a1c2cc13af55d2d7
haghshenas <mhgorgab@gmail.com>
2024-01-28 00:24:19 +0330

    added fillament admin panel

---
+++ app/Filament/Resources/MusicResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MusicResource\Pages;
+use App\Filament\Resources\MusicResource\RelationManagers;
+use App\Models\Music;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\CheckboxColumn;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MusicResource extends Resource
+{
+    protected static ?string $model = Music::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('cover')->label("کاور"),
+                Tables\Columns\TextColumn::make('title')->label("عنوان"),
+                Tables\Columns\TextColumn::make('slug')->label("آدرس"),
+                Tables\Columns\TextColumn::make('amount')->label("مبلغ")->default("دلخواه"),
+                IconColumn::make('is_active')->label("نمایش درسایت")
+                    ->boolean()
+                    ->trueIcon('heroicon-o-badge-check')
+                    ->falseIcon('heroicon-o-x-circle')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMusic::route('/'),
+            'create' => Pages\CreateMusic::route('/create'),
+            'edit' => Pages\EditMusic::route('/{record}/edit'),
+        ];
+    }
+}
